6bd457d9ccbba62aeb83f20009b4dd77c061fb14
Xiaoyong Sun <johnsunx1@gmail.com>
2011-12-27 00:00:00 +0000

    version 1.0.0

---
+++ R/global.r
@@ -0,0 +1,10 @@
+############################################
+##  R package cheatmap
+## function: global.r
+############################################
+
+
+cmapENV <- new.env()
+assign("matSepRow", 1, cmapENV)
+assign("totalLegendLabel", 5, cmapENV)
+
